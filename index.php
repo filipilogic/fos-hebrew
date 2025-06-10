5b0db5bf1d250348725047d9be50e944ad0918ef
filipilogic <filip@ilogic.co.il>
2025-06-10 20:48:08 +0200

    fix

--- index.php
+++ index.php
@@ -336,39 +336,86 @@ endif;
 				</div>
 
 				<!-- Bottom Section First -->
-				<div class="il_block il_video-popup-section info-box-width-60 " style=" --b-video-popup-space-top-ld: 10.4rem; --b-video-popup-space-bottom-ld: 10.4rem; --b-video-popup-space-top-mt: 10rem; --b-video-popup-space-bottom-mt: 10rem;">
-					<div class="il_section_inner container ib-left stack-mobile " style="--custom-max-width-ld: var(--site-width);">
-						<div class="il_block_intro align-left list-cols-1 " style="">
-							<div class="intro_before_title before-title-style-3 before-title-size-2 before-title-weight-700" style="--before-title-size-2-ld: 16px;--before-title-size-2-mt: 16px;--before_title_margin_bottom_ld: 1.6rem;--before_title_margin_bottom_mt: 10px;">ABOUT US</div>
-								<h2 class="intro_title title-style-2 title-size-2 title-weight-700" style="--title_margin_bottom_ld: 3.6rem;--title_margin_bottom_mt: 30px;">Why <span style="color: var(--color-3); font-weight: 400;">Sheba?</span></h2>
-								<div class="intro_text text-theme-color" style="--text_margin_bottom_mt: 40px;">
-									<p>The Canadian Friends of Sheba Medical Center is a registered charity based in Toronto committed to supporting the vital work of Sheba Medical Center, a global healthcare leader and the largest, most comprehensive hospital in Israel as well as the Middle East.</p>
-									<p>In addition to raising awareness and philanthropic support for Sheba’s humanitarian initiatives, compassionate care, cutting-edge research, and medical education activities, we are proud to serve as the medical center’s Canadian ambassador, helping foster collaborations that <b>serve the people of Canada, Israel, and the world.</b></p>
-									<p>It has been a privilege to contribute to Sheba’s transformative global efforts, and with your help, we will continue doing so for the benefit of patients everywhere.</p>
-								</div>
-
-								</div><div class="right">
-									<div class="column">
-									<a data-fancybox="video" data-type="iframe" data-preload="true" data-width="1270" data-height="720" href="https://www.youtube.com/watch?v=ge2hrlLL0qE" rel="lightbox">
-									<img loading="lazy" decoding="async" width="488" height="420" src="/wp-content/uploads/2023/11/Group-221.png" class="sec_desk_img" alt="" style="--bg-e-width-lg: 48.8rem; --bg-e-height-lg: 42rem; --bg-e-left-lg: auto; --bg-e-right-lg: auto; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 60rem; --bg-e-height-mt: auto; --bg-e-left-mt: auto; --bg-e-right-mt: auto; --bg-e-top-mt: 0; --bg-e-bottom-mt: 0; border-top-left-radius: 36px; border-top-right-radius: 36px; border-bottom-left-radius: 0px; border-bottom-right-radius: 36px;" srcset="/wp-content/uploads/2023/11/Group-221.png 488w, /wp-content/uploads/2023/11/Group-221-300x258.png 300w" sizes="(max-width: 488px) 100vw, 488px"></a>
-								</div>
+				<div class="il_block il_section" style="--b-space-top-ld: 10rem; --b-space-bottom-ld: 10rem; --b-space-top-mt: 10rem; --b-space-bottom-mt: 10rem;">
+					<div class="il_block_bg" style="background-color:">
+						<img loading="lazy" 
+							decoding="async" 
+							width="1386" 
+							height="576" 
+							src="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/06/Image-29-1.png" 
+							class="bg_element" 
+							alt="" 
+							style="--bg-e-width-lg: 138.6rem; --bg-e-height-lg: 57.6rem; --bg-e-left-lg: 0; --bg-e-right-lg: 0; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 52rem; --bg-e-height-mt: auto; --bg-e-left-mt: 0; --bg-e-right-mt: 0; --bg-e-top-mt: 0; --bg-e-bottom-mt: 0" 
+							srcset="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/06/Image-29-1.png 1386w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/06/Image-29-1-300x125.png 300w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/06/Image-29-1-1024x426.png 1024w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/06/Image-29-1-768x319.png 768w" 
+							sizes="auto, (max-width: 1386px) 100vw, 1386px">
+					</div>
+					<div class="il_section_inner container ib-custom-width stack-mobile content-align-right" style="--custom-max-width-ld: var(--site-width);">
+						<div class="il_block_intro align-right list-cols-1" style="flex-basis: 52%;">
+							<div class="intro_before_title before-title-style-3 before-title-size-1 before-title-weight-700" 
+								style="--before-title-size-1-ld: 16px; --before-title-size-1-mt: 16px; --before_title_margin_bottom_ld: 1.6rem; --before_title_margin_bottom_mt: 10px;">
+								על האגודה
+							</div>
+							<h2 class="intro_title title-style-2 title-size-3 title-weight-700" style="--title-size-3-ld: 4rem; --title-size-3-mt: 30px;">
+								ידידי <span style="color: var(--color-3);">שיבא</span>
+							</h2>
+							<div class="intro_subtitle subtitle-style-2 subtitle-size-1 subtitle-weight-700" style="color: #000000; --subtitle-size-1-ld: 18px; --subtitle-size-1-mt: 18px;">
+								כאן נולדה תנועה של אחריות רפואית וחברתית
+							</div>
+							<div class="intro_text text-black" style="--text_margin_bottom_ld: 5rem; --text_margin_bottom_mt: 30px;">
+								<p>בשיבא, החדשנות אינה רק יעד – היא מנוע פעולה יומיומי. שיבא אינו רק מוסד רפואי – הוא חממה לרעיונות פורצי דרך, למודלים טיפוליים חדשים ולשיתופי פעולה שמשפיעים על חייהם של מיליוני אנשים בישראל וברחבי העולם.</p>
+								<p>האגודה מהווה שגרירה של חזון שיבא בישראל, ומזמינה את הציבור לקחת חלק בעשייה הרפואית, המדעית והחברתית שמובילה את הרפואה קדימה – לטובת כל אחת ואחד מאיתנו.</p>
+							</div>
+							<div class="buttons">
+								<a class="il_btn button-color-green button-hover-color-green" href="#" target="_self">READ MORE</a>
+							</div>
 						</div>
 					</div>
 				</div>
 
 
 				<!-- Bottom CTA Section -->
-				<div class="il_block il_section hp-helping-donate-now-section " style=" --b-space-top-ld: 6.5rem; --b-space-bottom-ld: 8.1rem; --b-space-top-mt: 10rem; --b-space-bottom-mt: 32rem;">
+				<div class="il_block il_section hp-helping-donate-now-section" style="--b-space-top-ld: 6.5rem; --b-space-bottom-ld: 8.1rem; --b-space-top-mt: 10rem; --b-space-bottom-mt: 32rem;">
 					<div class="il_block_bg" style="background: linear-gradient(96deg, #053279 0%, #030B27 99.13%);">
-					<img loading="lazy" decoding="async" width="670" height="360" src="/wp-content/uploads/2023/10/Image-18.png" class="bg_element" alt="" style="--bg-e-width-lg: auto; --bg-e-height-lg: 100%; --bg-e-left-lg: auto; --bg-e-right-lg: 0; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 33.5rem; --bg-e-height-mt: 18rem; --bg-e-left-mt: 0; --bg-e-right-mt: 0; --bg-e-top-mt: 0; --bg-e-bottom-mt: 0; --bg-e-display-mobile: none" srcset="/wp-content/uploads/2023/10/Image-18.png 670w, /wp-content/uploads/2023/10/Image-18-300x161.png 300w" sizes="(max-width: 670px) 100vw, 670px"><img loading="lazy" decoding="async" width="955" height="784" src="/wp-content/uploads/2023/12/Donate-now_image_mobile-2.png" class="bg_element" alt="" style="--bg-e-width-lg: 95.5rem; --bg-e-height-lg: 78.4rem; --bg-e-left-lg: 0; --bg-e-right-lg: 0; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 40rem; --bg-e-height-mt: auto; --bg-e-left-mt: auto; --bg-e-right-mt: 0; --bg-e-top-mt: auto; --bg-e-bottom-mt: 0; --bg-e-display-desktop: none" srcset="/wp-content/uploads/2023/12/Donate-now_image_mobile-2.png 955w, /wp-content/uploads/2023/11/Donate-now_image_mobile-2-300x246.png 300w, /wp-content/uploads/2023/11/Donate-now_image_mobile-2-768x630.png 768w" sizes="(max-width: 955px) 100vw, 955px">
+						<img loading="lazy" 
+							decoding="async" 
+							width="1920" 
+							height="360" 
+							src="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1.jpg" 
+							class="desk_bg" 
+							alt="" 
+							srcset="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1.jpg 1920w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1-300x56.jpg 300w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1-1024x192.jpg 1024w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1-768x144.jpg 768w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donatebg1-1536x288.jpg 1536w" 
+							sizes="auto, (max-width: 1920px) 100vw, 1920px">
+						<img loading="lazy" 
+							decoding="async" 
+							width="670" 
+							height="359" 
+							src="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donateimg.png" 
+							class="bg_element" 
+							alt="" 
+							style="--bg-e-width-lg: auto; --bg-e-height-lg: 100%; --bg-e-left-lg: 0; --bg-e-right-lg: auto; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 33.5rem; --bg-e-height-mt: 17.95rem; --bg-e-left-mt: 0; --bg-e-right-mt: 0; --bg-e-top-mt: 0; --bg-e-bottom-mt: 0; --bg-e-display-mobile: none" 
+							srcset="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donateimg.png 670w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/donateimg-300x161.png 300w" 
+							sizes="auto, (max-width: 670px) 100vw, 670px">
+						<img loading="lazy" 
+							decoding="async" 
+							width="709" 
+							height="609" 
+							src="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/Donate-now_image_mobile-1.png" 
+							class="bg_element" 
+							alt="" 
+							style="--bg-e-width-lg: 70.9rem; --bg-e-height-lg: 60.9rem; --bg-e-left-lg: 0; --bg-e-right-lg: 0; --bg-e-top-lg: 0; --bg-e-bottom-lg: 0; --bg-e-width-mt: 50rem; --bg-e-height-mt: auto; --bg-e-left-mt: 0; --bg-e-right-mt: auto; --bg-e-top-mt: auto; --bg-e-bottom-mt: 0; --bg-e-display-desktop: none" 
+							srcset="https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/Donate-now_image_mobile-1.png 709w, https://hebrew-fos.ilogic-dev.net/wp-content/uploads/2025/05/Donate-now_image_mobile-1-300x258.png 300w" 
+							sizes="auto, (max-width: 709px) 100vw, 709px">
 					</div>
-					<div class="il_section_inner container ib-fullwidth stack-mobile " style="--custom-max-width-ld: var(--site-width);">
-						<div class="il_block_intro align-left " style="">
-							<h2 class="intro_title title-style-1 title-size-2 title-weight-700" style="--title-size-2-ld: 3.6rem;--title-size-2-mt: 25px;--title_margin_bottom_ld: 5rem;--title_margin_bottom_mt: 30px;"><span>Whether helping those most in need or advancing healthcare globally: </span><br>when you support Sheba, you are making a difference.</h2>
+					<div class="il_section_inner container ib-fullwidth stack-mobile" style="--custom-max-width-ld: var(--site-width);">
+						<div class="il_block_intro align-right">
+							<h2 class="intro_title title-style-2 title-size-2 title-weight-700" style="--title-size-2-ld: 3.6rem; --title-size-2-mt: 25px; --title_margin_bottom_ld: 5rem; --title_margin_bottom_mt: 30px;">
+								<span>זה הרבה יותר מתרומה. זו שותפות אמיתית בריפוי.</span><br>
+								הצטרפו לקהילת התורמים שלנו והשפיעו יחד איתנו
+							</h2>
 							<div class="buttons">
-									<a class="il_btn  button-color-green button-hover-color-pink" href="<?php echo $archive_cta_button_link_url; ?>" target="_self"><?php echo $archive_cta_button_link_title; ?></a>
+								<a class="il_btn button-color-green button-hover-color-pink" href="/donate/" target="_self">תירמו עכשיו</a>
 							</div>
-						</div>	
+						</div>
 					</div>
 				</div>
 			</div>
